61d37d686de366d9d97f1e9bf580cf8be7313c20
juanma-ge <jgomest545@g.educaand.es>
2025-05-14 22:29:36 +0200

    Insercción de dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
 
 tasks.test {
---
+++ src/test/kotlin/services/actividadServicesTest.kt
@@ -0,0 +1,21 @@
+package services
+
+import es.prog2425.taskmanager.datos.ActividadRepository
+import es.prog2425.taskmanager.modelo.Actividad
+import es.prog2425.taskmanager.modelo.Estado
+import es.prog2425.taskmanager.modelo.Evento
+import es.prog2425.taskmanager.modelo.Tarea
+import es.prog2425.taskmanager.servicios.ActividadService
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.lang.IllegalArgumentException
+
+class actividadServicesTest {
+
+
+
+}
